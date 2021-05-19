f7b96411884c46e3327199d21621a15cc8213312
Johan Castillo <jcjohan2707@gmail.com>
2021-05-19 15:53:42 -0400

    Carousel dynamic with data JSON

--- sliders/5/carousel-react/src/App.js
+++ sliders/5/carousel-react/src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 import Carousel from './components/Carousel';
+import data from "./data.json"
 
 function App() {
   return (
    <>
-    <Carousel />
+   
+    <Carousel 
+      title="Tecnología"
+      link="https://compralotodo.com"
+      products={data}
+    />
+    
    </> 
   )
 }
--- sliders/5/carousel-react/src/components/Carousel.jsx
+++ sliders/5/carousel-react/src/components/Carousel.jsx
@@ -1,7 +1,9 @@
-import data from "../data.json"
 
 
-const Carousel = () => {
+const Carousel = ({title, products, link}) => {
+
+
+    
 
 
     let prevAction = (leftPosition, slickWidth, track) => {
@@ -42,7 +44,11 @@ const Carousel = () => {
 
 
     return (
-        <div className="Carousel">
+        <div className="carousel">
+
+            <h4 className="title">
+                <a href={link}>{title}</a>
+            </h4>
 
             <div className="slick-list" id="slick-list">
 
@@ -53,7 +59,7 @@ const Carousel = () => {
                 <div className="slick-track" id="track">
 
                     {
-                        data.map(product => {
+                        products.map(product => {
                             return (
                                 <div className="slick" key={product.id}>
                                     <div>
@@ -81,39 +87,6 @@ const Carousel = () => {
 
             </div>
 
-            <div className="slick-list" id="slick-list">
-
-                <button className="slick-arrow slick-prev" id="button-prev" data-button="button-prev" onClick={e => processingButton(e)}>
-                    <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="chevron-left" className="svg-inline--fa fa-chevron-left fa-w-10" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path fill="currentColor" d="M34.52 239.03L228.87 44.69c9.37-9.37 24.57-9.37 33.94 0l22.67 22.67c9.36 9.36 9.37 24.52.04 33.9L131.49 256l154.02 154.75c9.34 9.38 9.32 24.54-.04 33.9l-22.67 22.67c-9.37 9.37-24.57 9.37-33.94 0L34.52 272.97c-9.37-9.37-9.37-24.57 0-33.94z" /></svg>
-                </button>
-
-                <div className="slick-track" id="track">
-
-                    {
-                        data.map(product => {
-                            return (
-                                <div className="slick" key={product.id}>
-                                    <div>
-                                        <a href="/">
-
-                                            <picture>
-                                                <img src={product.image} alt="Image" />
-                                            </picture>
-                                        </a>
-                                    </div>
-                                </div>
-                            )
-                        })
-                    }
-
-
-                </div>
-
-                <button className="slick-arrow slick-next" id="button-next" data-button="button-next" onClick={e => processingButton(e)}>
-                    <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="chevron-right" className="svg-inline--fa fa-chevron-right fa-w-10" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path fill="currentColor" d="M285.476 272.971L91.132 467.314c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505 256 34.484 101.255c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0L285.475 239.03c9.373 9.372 9.373 24.568.001 33.941z" /></svg>
-                </button>
-
-            </div>
         </div>
 
     )
